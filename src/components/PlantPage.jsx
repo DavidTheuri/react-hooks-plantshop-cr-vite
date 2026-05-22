cce42a0cc37da3d9070af27ef1d8bd58cbcec5e3
David Theuri <davidtheuri19495@gmail.com~>
2026-05-22 17:56:38 +0300

    code updated .

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([]);
+
+  const [search, setSearch] = useState("");
+
+
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+
+
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+
+
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+
+      <NewPlantForm onAddPlant={handleAddPlant} />
+
+      <Search
+        search={search}
+        setSearch={setSearch}
+      />
+
+      <PlantList plants={filteredPlants} />
+
     </main>
   );
 }
